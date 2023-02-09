cdf44035939dc139a3bf62b7eec85f3de7190412
Your Name <you@example.com>
2023-02-09 11:33:07 +0500

    add init page

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -4,6 +4,7 @@ const Overvienw = () => {
   return (
     <div className=' text-center offset-5   p-5 shadow  w-25 '>
       <h1>NOT FONDE</h1>
+      <h5> ^ Tickets-Pagega-O'ting</h5>
     </div>
   )
 }
